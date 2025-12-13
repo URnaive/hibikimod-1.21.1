ee22e594dce894235699af60df7bbc343fdb174f
Vernyi_226 <30955161+URnaive@users.noreply.github.com>
2025-12-14 00:10:46 +0800

    Initial commit

---
+++ src/main/java/com/hibiki/pack/HibikiModClient.java
@@ -0,0 +1,11 @@
+package com.hibiki.pack;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HibikiModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
